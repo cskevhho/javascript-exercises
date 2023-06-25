934abbec027fe07cc9d4b1a0664d4a91e72b4984
Kevin <kevhho@my.yorku.ca>
2023-06-25 14:11:45 -0400

    First half of assignment bit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    
+    const tempArr = [];
 
-};
-
+    arr.forEach((item) => {
+        if (!args.includes(arr)) {
+            tempArr.push(item);
+        }
+    });
+}
 // Do not edit below this line
 module.exports = removeFromArray;
