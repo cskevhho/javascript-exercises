b983657bb826d0ad932a1ab78359e38137bcc62f
Kevin <kevhho@my.yorku.ca>
2023-06-25 15:00:26 -0400

    Implemented second half of functions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+  let res = 0;
+  let lowEnd, highEnd;
 
+  if (typeof firstNum === "number" && typeof secondNum === "number") {
+    if (firstNum > 0 && secondNum > 0) {
+      if (firstNum < secondNum) {
+        lowEnd = firstNum;
+        highEnd = secondNum;
+      } else {
+        highEnd = firstNum;
+        lowEnd = secondNum;
+      }
+
+      for (let i = lowEnd; i < highEnd + 1; i++) {
+        res += i;
+      }
+      return res;
+    } else {
+      return "ERROR";
+    }
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
